edee5a145a57cecbcedad680efdfa5e68a92b205
Shivang Nagaria <shivang.nagaria@gmail.com>
2021-03-03 23:06:21 +0530

    Specified better function names

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -5,18 +5,19 @@ class ProxyController < ActionController::API
 
   def index
     git = Git::Session.new
+    proxy_request_params = generate_proxy_request_params
     response = git.make_request(proxy_request_params)
     render json: response.body, status: response.status
   end
 
   private
-  # TODO: Can we directly pass request to Git::Session & let it figure out stuff?
-  # we are actually parsing request & params to get what we need
-  def proxy_request_params
+
+  # Constructing proxy_request params
+  def generate_proxy_request_params
     {
       endpoint: params[:path],
       method: normalize_method(request.method),
-      body: request.raw_post, # Pass whatever body you get from request
+      body: request.raw_post, # Pass whatever body you get from the request
       headers: parse_headers(request),
       params: parse_query_params(params)
     }
--- app/helpers/proxy_helper.rb
+++ app/helpers/proxy_helper.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 module ProxyHelper
-  # Only taking Accept & Authorization header
-  # As Rails is mixing its own headers, it is not possible to distinguish between user defined & rails defined headers
-  # Also, we can't ask user to only send Accept & Authorization headers when they send other kind of headers because
-  # we don't know if they are actually sent by the user or set by Rails
+  # Only taking Accept & Authorization header. As Rack is mixing its own headers, it is a bit hacky to distinguish
+  # between user defined & rails defined headers. Also, we can't ask user to only send Accept & Authorization headers
+  # when they send other kind of headers due to the same reason.
+  # Read more:
+  # Headers with `HTTP_` prefix are user-generated headers https://stackoverflow.com/a/6318491/4657277
   def parse_headers(request)
     headers = {}
-    # is it mutability?
     %w[Accept Authorization].each do |key|
-      headers[key] = request.headers[key] if request.headers.key?(key)
+      next unless request.headers.key?(key)
+      headers[key] = request.headers[key]
     end
     headers
   end
@@ -18,7 +19,6 @@ module ProxyHelper
   # we will need to update this code if the list changes in future
   def parse_query_params(params)
     query_params = {}
-    # is it mutability?
     params.each do |key, value|
       next if %w[controller action path].include?(key)
       query_params[key] = value
@@ -33,7 +33,6 @@ module ProxyHelper
     unless method.is_a?(String)
       raise ArgumentError, "Invalid method type. Expected String, found #{method.class}"
     end
-    # Is it mutation?
     method.downcase.to_sym
   end
 end
