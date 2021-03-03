bb8e89d12b235dfbe5db097488e4159dc86dcd51
Shivang Nagaria <shivang.nagaria@gmail.com>
2021-03-04 00:09:20 +0530

    removed stub_helper

--- spec/lib/external_api/base_spec.rb
+++ spec/lib/external_api/base_spec.rb
@@ -1,48 +1,43 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "request_helper"
-
-def init_ext_api(base_url)
-  ExternalApi::Base.new(base_url: base_url)
-end
 
 RSpec.describe ExternalApi::Base do
   ExternalApi::METHODS.each do |method|
-    base_url = base_url_generator
-    describe "#{method} on randomly generated endpoint" do
-      context "when requested" do
-        it "hit correct url with correct header" do
+    base_url = Faker::Internet.url(path: nil) # test should be isolated. Let's move it inside text itself??
+    describe "On randomly generated URL" do
+      context "when #{method.upcase} request is performed" do
+        it "hits correct URL with correct headers" do
           # setup
-          endpoint, request_headers = request_params
-          stub_request_with_base_url(method, base_url, endpoint)
+          endpoint = FactoryBot.build(:endpoint)
+          headers = FactoryBot.build(:headers)
+          url = "#{base_url}/#{endpoint}"
+          stub_request(method, url)
 
-          # test
-          ext_api = init_ext_api(base_url)
-          ext_api.make_request(endpoint: endpoint, method: method, headers: request_headers)
+          # exercise
+          ext_api = ExternalApi::Base.new(base_url: base_url)
+          ext_api.make_request(endpoint: endpoint, method: method, headers: headers)
 
-          # except
+          # verify
           # TODO: Other headers are also coming (User-Agent, Accept-Encoding, etc). What to do about it?
-          expect(WebMock).to have_requested(method, "#{base_url}/#{endpoint}").with(headers: request_headers)
+          expect(WebMock).to have_requested(method, url).with(headers: headers)
         end
-      end
 
-      context "when requested" do
-        it "hit correct url with correct header and return correct response" do
+        it "hits correct URL with correct headers and return correct response" do
           # setup
-          endpoint, request_headers = request_params
+          endpoint = FactoryBot.build(:endpoint)
+          headers = FactoryBot.build(:headers)
+          url = "#{base_url}/#{endpoint}"
           expected_response_body = Faker::Types.rb_hash
 
-          stub_request_with_base_url(method, base_url, endpoint)
-            .to_return(body: expected_response_body.to_json, headers: request_headers)
+          stub_request(method, url).to_return(body: expected_response_body.to_json, headers: headers)
 
-          # test
-          ext_api = init_ext_api(base_url)
-          response = ext_api.make_request(endpoint: endpoint, method: method, headers: request_headers)
+          # exercise
+          ext_api = ExternalApi::Base.new(base_url: base_url)
+          response = ext_api.make_request(endpoint: endpoint, method: method, headers: headers)
 
-          # Note: This test case is failing randomly. Haven't figure out any pattern yet
-          # except
-          expect(WebMock).to have_requested(method, "#{base_url}/#{endpoint}").with(headers: request_headers)
+          # verify
+          expect(WebMock).to have_requested(method, url).with(headers: headers)
           expect(response.body).to eq(expected_response_body.to_json)
         end
       end
@@ -50,45 +45,48 @@ RSpec.describe ExternalApi::Base do
   end
 
   ExternalApi::METHODS_WITH_PARAMS.each do |method|
-    base_url = base_url_generator
-    describe "#{method} on randomly generated endpoint" do
-      context "when requested with params" do
-        it "hit correct url with params" do
+    base_url = Faker::Internet.url(path: nil)
+    describe "On randomly generated URL" do
+      context "when #{method.upcase} request is performed" do
+        it "hits correct URL with correct params" do
           # setup
-          endpoint, request_headers = request_params
+          endpoint = FactoryBot.build(:endpoint)
+          headers = FactoryBot.build(:headers)
           opts = Faker::Types.rb_hash
-          stub_request_with_base_url(method, base_url, endpoint).with(query: opts)
+          url = "#{base_url}/#{endpoint}"
+          stub_request(method, url).with(query: opts)
 
-          # test
-          ext_api = init_ext_api(base_url)
-          ext_api.make_request(endpoint: endpoint, method: method, headers: request_headers, params: opts)
+          # exercise
+          ext_api = ExternalApi::Base.new(base_url: base_url)
+          ext_api.make_request(endpoint: endpoint, method: method, headers: headers, params: opts)
 
-          # except
-          expect(WebMock).to have_requested(method, "#{base_url}/#{endpoint}").with(query: opts, headers: request_headers)
+          # verify
+          expect(WebMock).to have_requested(method, url).with(query: opts, headers: headers)
         end
       end
     end
   end
 
   ExternalApi::METHODS_WITH_BODY.each do |method|
-    base_url = base_url_generator
-    describe "#{method} on randomly generated endpoint" do
-      context "when requested with body" do
-        it "hit correct url with correct body" do
+    base_url = Faker::Internet.url(path: nil)
+    describe "On randomly generated URL" do
+      context "when #{method.upcase} request is performed" do
+        it "hits correct URL with correct body" do
           # setup
-          endpoint, request_headers = request_params
+          endpoint = FactoryBot.build(:endpoint)
+          headers = FactoryBot.build(:headers)
           body = JSON.generate(Faker::Types.rb_hash)
-          # Is appending .with ok here?
-          stub_request_with_base_url(method, base_url, endpoint)
+          url = "#{base_url}/#{endpoint}"
+          stub_request(method, url)
 
-          # test
-          ext_api = init_ext_api(base_url)
-          ext_api.make_request(endpoint: endpoint, method: method, headers: request_headers, body: body)
+          # exercise
+          ext_api = ExternalApi::Base.new(base_url: base_url)
+          ext_api.make_request(endpoint: endpoint, method: method, headers: headers, body: body)
 
-          # except
+          # verify
           # TODO: Make it one_time
-          expect(WebMock).to have_requested(method, "#{base_url}/#{endpoint}")
-            .with(headers: request_headers, body: body)
+          expect(WebMock).to have_requested(method, url)
+            .with(headers: headers, body: body)
         end
       end
     end
